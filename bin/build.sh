b7bd0d86f0defa74ccdebd97b04bf91797177ad2
Tony Findeisen <github@it-ony.de>
2017-04-03 23:12:59 +0200

    added build script

---
+++ bin/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${DIR} && cd ..
+
+zip --exclude=*.DS_Store* --exclude=*bin* --exclude=*.git* --exclude=LICENSE --exclude=README.md -r draaft-chrome-extension.zip .
+cd $PWD
+
+
+
