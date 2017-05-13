b9bbc9c9308ad8358b823e12eb1fd774c88f1eb8
Tony Findeisen <github@it-ony.de>
2017-05-13 15:27:17 +0200

    updated build script to only include files that are necessary

--- bin/build.sh
+++ bin/build.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${DIR} && cd ..
 
-zip --exclude=*.DS_Store* --exclude=*bin* --exclude=*.git* --exclude=*.iml --exclude=LICENSE --exclude=README.md -r draaft-chrome-extension.zip .
+zip -r draaft-chrome-extension.zip -@ < bin/files
 cd $PWD
 
 
